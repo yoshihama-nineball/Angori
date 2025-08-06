ab6eace7583c445c32bf73d05de54defab4dc75d
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-06 17:15:55 +0900

    fix: layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,43 +1,15 @@
+// app/home/layout.tsx
 import { Metadata } from 'next'
-import React from 'react'
-// import Loading from "./components/feedback//Loading/Loading";
-import Footer from '../components/layouts/Footer/Footer'
-// import Header from "../components/layouts/Header/Header";
-import { ClientThemeProvider } from './components/layouts/ClientThemeProvider'
 
 export const metadata: Metadata = {
-  title: 'アンガーアプリ | TOP',
-  description: 'TOPページです',
-  icons: {
-    icon: '/icon.png',
-  },
+  title: 'アンゴリ | Home',
+  description: 'Homeのページです',
 }
 
-export default function RootLayout({
+export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="ja">
-      <body>
-        <ClientThemeProvider>
-          <div>ヘッダー</div>
-          {/* メインコンテンツをラップして、ヘッダーの高さ分のパディングを追加 */}
-          <main
-            style={{
-              paddingTop: 'var(--header-height, 64px)', // デフォルト値として64pxを設定
-              minHeight: 'calc(100vh - var(--footer-height, 56px))', // フッターの高さを考慮
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            {/* <React.Suspense fallback={<Loading />}>{children}</React.Suspense> */}
-            <React.Suspense>{children}</React.Suspense>
-          </main>
-          <Footer />
-        </ClientThemeProvider>
-      </body>
-    </html>
-  )
+  return <>{children}</>
 }
