159f0f80645336410d6ae9c7fde42cff37d01567
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-07 18:08:36 +0900

    add: layout.tsx

---
+++ frontend/src/app/terms/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'アンゴリ | 利用規約',
+  description: '利用規約のページです',
+}
+
+export default function TermsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
