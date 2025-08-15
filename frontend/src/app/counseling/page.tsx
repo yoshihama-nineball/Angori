52ccaa25993a375cb711b81c78ca9f67d653655a
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-15 11:03:33 +0900

    add: counseling page

---
+++ frontend/src/app/counseling/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import AuthGuard from '@/components/auth/AuthGuard'
+import CounselingLayout from '@/components/counseling/CounselingLayout'
+
+const CounselingPage = () => {
+  return (
+    <AuthGuard>
+      <CounselingLayout />
+    </AuthGuard>
+  )
+}
+
+export default CounselingPage
