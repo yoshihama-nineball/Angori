0850224a3a90e14b0937bf4697ba47c5a25d65cb
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-16 21:14:18 +0900

    fix:rubocop

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # シンプルに戻す
-    
+    origins '*' # シンプルに戻す
+
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
