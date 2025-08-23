65b9dd396ceeead903496c0c1ce818561815bd00
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-23 13:06:57 +0900

    add: (auth) folder

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main style={{ minHeight: '100vh' }}>
+      {children}
+    </main>
+  )
+}
