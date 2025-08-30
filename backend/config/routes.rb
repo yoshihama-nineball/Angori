266317033c4d79041a23a763c9c21d35475d61bd
yoshihama-nineball <burimofumofu@gmail.com>
2025-08-30 17:34:08 +0900

    fix: routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # ヘルスチェック用のルートパス
+  root to: proc { [200, {}, ['OK']] }
+
   get 'up' => 'rails/health#show', as: :rails_health_check
   get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
