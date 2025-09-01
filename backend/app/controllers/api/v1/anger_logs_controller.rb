c45293b25cb253094b86c00c09cab29c15a03016
yoshihama-nineball <burimofumofu@gmail.com>
2025-09-01 13:12:32 +0900

    add: omniauth

--- backend/app/controllers/api/v1/anger_logs_controller.rb
+++ backend/app/controllers/api/v1/anger_logs_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class AngerLogsController < ApplicationController
       include AngerLogsConcern
+
       before_action :set_anger_log, only: %i[show update destroy]
 
       # GET /api/v1/anger_logs
---
+++ backend/db/migrate/20250830024546_add_omniauth_to_users.rb
@@ -0,0 +1,4 @@
+class AddOmniauthToUsers < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
